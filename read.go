9c3974a37d4f9413e255675a679d8a979a22b467
Ben Pate <ben@pate.org>
2023-09-10 10:05:06 -0600

    Add ReplaceBody function

--- read.go
+++ read.go
@@ -14,20 +14,31 @@ import (
 // https://medium.com/@xoen/golang-read-from-an-io-readwriter-without-loosing-its-content-2c6911805361
 func ReadBody(request *http.Request) ([]byte, error) {
 
+	reader, err := ReplaceBody(request)
+
+	if err != nil {
+		return []byte{}, derp.Wrap(err, "re.ReadBody", "Error reading request body")
+	}
+
+	return reader.Bytes(), nil
+}
+
+// ReplaceBody replaces the request.Body with a re.Reader.
+func ReplaceBody(request *http.Request) (Reader, error) {
+
 	// If we already have a re.Reader, then read away.
 	if reader, ok := request.Body.(Reader); ok {
-		return reader.Bytes(), nil
+		return reader, nil
 	}
 
 	// Otherwise, shim the re.Reader into the request.Body
 	newReader, err := NewReader(request.Body)
 
 	if err != nil {
-		return nil, derp.Wrap(err, "re.ReadBody", "Error reading request body")
+		return Reader{}, derp.Wrap(err, "re.ReadBody", "Error reading request body")
 	}
 
 	request.Body = newReader
 
-	// Then return the body as a slice of bytes
-	return newReader.Bytes(), nil
+	return newReader, nil
 }
